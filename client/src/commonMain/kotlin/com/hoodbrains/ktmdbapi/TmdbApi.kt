a67935f9433df5be5b2b9c8572fc1dd4a90d0d93
Vincent Guilhoto <vincent@MBP-de-Vincent.local>
2021-03-17 08:56:18 +0100

    feature(genre): adding models and interface.

--- client/src/commonMain/kotlin/com/hoodbrains/ktmdbapi/TmdbApi.kt
+++ client/src/commonMain/kotlin/com/hoodbrains/ktmdbapi/TmdbApi.kt
@@ -1,10 +1,12 @@
 package com.hoodbrains.ktmdbapi
 
 import com.hoodbrains.ktmdbapi.model.configuration.*
+import com.hoodbrains.ktmdbapi.model.genre.Genre
 
 interface TmdbApi {
 
     val configuration : ConfigurationApi
+    val genre : GenreApi
 
     interface ConfigurationApi {
         suspend fun fetchMain(): Configuration
@@ -13,5 +15,10 @@ interface TmdbApi {
         suspend fun fetchLanguages(): List<Language>
         suspend fun fetchPrimaryTranslations(): List<String>
         suspend fun fetchTimezones(): List<Timezone>
+    }
+
+    interface GenreApi {
+        suspend fun movieGenreList(): List<Genre>
+        suspend fun tvGenreList(): List<Genre>
     }
 }
---
+++ client/src/commonMain/kotlin/com/hoodbrains/ktmdbapi/model/genre/Genre.kt
@@ -0,0 +1,9 @@
+package com.hoodbrains.ktmdbapi.model.genre
+
+/**
+ * https://developers.themoviedb.org/3/genres/get-movie-list
+ */
+data class Genre(
+    val id: Int,
+    val name : String
+)
