3003ffb48d245170816da542689a1652025db11a
Vincent Guilhoto <vincent@MBP-de-Vincent.local>
2021-03-25 22:26:00 +0100

    feature(cli) : adding base cli project

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,17 +1,21 @@
 plugins {
-    java
+    kotlin("jvm").version("1.4.30")
+    application
+
 }
 
-group = "com.hoobrains.ktmdbapi"
+group = "com.hoodbrains.ktmdbapi.cli"
 version = "0.1"
 
+application {
+    mainClass.set("com.hoodbrains.ktmdbapi.cli.TmdbApiCliKt")
+}
 repositories {
     mavenCentral()
+
 }
 
 dependencies {
     testImplementation("junit", "junit", "4.12")
     implementation("com.github.ajalt.clikt:clikt:3.1.0")
-    implementation(project(":client"))
 }
-
